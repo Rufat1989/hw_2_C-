6c15443ecc3e056b6f4845068a0c1114d14bb3a3
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-17 17:21:24 +0600

    second task

---
+++ task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if(n / 100 == 0)
+{
+    Console.Write ($"{n}, третьего числа нет");
+
+}
+
+else
+{
+    while (n > 999)
+{
+    n = n / 10;
+    
+}
+Console.Write ($"{n}, третье число {n % 10}");
+}
+
+
+
+
+
