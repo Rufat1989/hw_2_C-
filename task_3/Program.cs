5d5ee0155d33fd17776e31f698fbd444f2b5e851
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-19 11:20:25 +0600

    third task

---
+++ task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int d = int.Parse(Console.ReadLine()!);
+if(d == 6 || d == 7)
+{
+    Console.Write($"{d}  -->  выходной");
+
+}
+else
+{
+    Console.Write($"{d}  -->  не выходной");
+}
